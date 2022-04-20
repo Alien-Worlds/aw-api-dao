3acb611cf72df1b2fdfc671f4a4783a627b2390f
rkamysz <radoslaw.kamysz@gmail.com>
2022-04-20 17:11:47 +0200

    change endpoints

--- local.config.js
+++ local.config.js
@@ -11,23 +11,31 @@ module.exports = {
         connectionString: 'amqp://guest:guest@queue_mainnet/'
     },
     ws: {
-        host: 'localhost',
-        port: '3030'
+        host: 'ws',
+        port: '3031'
     },
     ipc: {
         id: 'livenotifications',
         appspace: 'eosdac.'
     },
     eos: {
+        // contracts: ['token.worlds', 'dao.worlds', 'msig.worlds', 'alien.worlds', 'eyeke.world', 'kavian.world', 'magor.world', 'naron.world', 'neri.world', 'veles.world'],
         chainId: "8be32650b763690b95b7d7e32d7637757a0a7392ad04f1c393872e525a2ce82b",
-        endpoint: 'https://wax.eosdac.io/',
-        wsEndpoint: 'ws://localhost:8080',
-        msigContract: 'msig.world',
-        dacGenesisBlock: 1,  // the first block that includes any dac contract actions including the initial setcode
-        dacDirectoryContract: 'dacdirectory',
+        // endpoint: 'http://127.0.0.1:38888',
+        // wsEndpoint: 'ws://127.0.0.1:38080',
+        // wsEndpoints: ['ws://127.0.0.1:38080'],
+        endpoint: 'https://wax.eosdac.io',
+        wsEndpoint: 'ws://waxnode-ovh-pl-01.alienworlds.io:28080',
+        wsEndpoints: ['ws://waxnode-ovh-pl-01.alienworlds.io:28080'],
+        msigContract: 'msig.worlds',
+        custodianContract: 'dao.worlds',
+        dacDirectoryContract: 'index.worlds',
         legacyDacs: ['eos.dac'],
-        dacDirectoryMode: 'all',
-        dacDirectoryDacId: ''
+        // the first block that includes any dac contract actions including the initial setcode
+        // dao.worlds contract was first set 105376981 
+        // 105578904 - first stprofile block
+        // dacGenesisBlock: 123428449,
+        dacGenesisBlock: 176000000,
     },
     logger: {
         level: "info",
